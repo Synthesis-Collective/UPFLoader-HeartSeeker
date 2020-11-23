ae18e13797bd9c8ac439c0e25fe22c4e3f20f4ed
Fuzzles <t.brown.work@gmail.com>
2020-11-22 22:46:20 -0800

    tuning and updates

--- HeartSeekerUPFLoader/Program.cs
+++ HeartSeekerUPFLoader/Program.cs
@@ -2,6 +2,7 @@ using DynamicData;
 using Mutagen.Bethesda;
 using Mutagen.Bethesda.Skyrim;
 using Mutagen.Bethesda.Synthesis;
+using Noggog;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -39,13 +40,7 @@ namespace HeartSeekerUPFLoader
              * check if exceeding 254 plugin load and warn
              * clean up, tighten up
              * investigate mutagen 0.21.3 load order calling
-             * investigate linked formkeys (eg ammo.LinkedFormKeys)
-             * +Noggog: you might want to remove ModKey.Null from the set after running, as that'll likely be added to your set, thanks to any null FormLinks
-             * related to LinkedFormKey, make sure bash/smash included
-             
              */
-
-
 
 
 
@@ -57,87 +52,103 @@ namespace HeartSeekerUPFLoader
                 System.Console.WriteLine($"Please add {mk} to your load order");
                 return;
             }
+            
+            // got it, carrying on
+            //System.Console.WriteLine($"{mk} found! Loading...");
 
-            System.Console.WriteLine($"{mk} found! Loading...");
-
+            
             //create hash set to hold mods we want
             var modKeySet = new HashSet<ModKey>();
+            
 
             //detect ammunition records in mods, adds mods containing those records to set
             foreach (var ammoGetter in state.LoadOrder.PriorityOrder.Ammunition().WinningOverrides())
             {
+                //pull the mod names from the FormKey
                 var modKey = ammoGetter.FormKey.ModKey;
+                //pull the mod names from linked records
+                var linkedKeys = ammoGetter.LinkFormKeys.Select(l => l.ModKey);
 
-                if (modKeySet.Add(modKey))
-                {
-                    System.Console.Write(modKey + " added." + "\n");
-                }
+                //add keys to our hashset
+                modKeySet.Add(modKey);
+                modKeySet.Add(linkedKeys);
+
             }
 
-            //detect weapon records in mods, adds mods containing those records to set
+            //detect projectile records in mods, adds mods containing those records to set. 
+            //heartseeker doesn't specifically call for, but safer to include
             foreach (var projGetter in state.LoadOrder.PriorityOrder.Projectile().WinningOverrides())
             {
                 var modKey = projGetter.FormKey.ModKey;
+                var linkedKeys = projGetter.LinkFormKeys.Select(l => l.ModKey);
 
-                if (modKeySet.Add(modKey))
-                {
-                    System.Console.Write(modKey + " added." + "\n");
-                }
+                //add keys to our hashset
+                modKeySet.Add(modKey);
+                modKeySet.Add(linkedKeys);
             }
 
             //detect weapon records in mods, adds mods containing those records to set
             foreach (var weapGetter in state.LoadOrder.PriorityOrder.Weapon().WinningOverrides())
             {
                 var modKey = weapGetter.FormKey.ModKey;
+                var linkedKeys = weapGetter.LinkFormKeys.Select(l => l.ModKey);
 
-                if (modKeySet.Add(modKey))
-                {
-                    System.Console.Write(modKey + " added." + "\n");
-                }
+                //add keys to our hashset
+                modKeySet.Add(modKey);
+                modKeySet.Add(linkedKeys);
             }
 
             //detect spell records in mods, adds mods containing those records to set
             foreach (var spellGetter in state.LoadOrder.PriorityOrder.Spell().WinningOverrides())
             {
                 var modKey = spellGetter.FormKey.ModKey;
+                var linkedKeys = spellGetter.LinkFormKeys.Select(l => l.ModKey);
 
-                if (modKeySet.Add(modKey))
-                {
-                    System.Console.Write(modKey + " added." + "\n");
-                }
+                //add keys to our hashset
+                modKeySet.Add(modKey);
+                modKeySet.Add(linkedKeys);
             }
 
             //detect perk records in mods, adds mods containing those records to set
             foreach (var perkGetter in state.LoadOrder.PriorityOrder.Perk().WinningOverrides())
             {
                 var modKey = perkGetter.FormKey.ModKey;
+                var linkedKeys = perkGetter.LinkFormKeys.Select(l => l.ModKey);
 
-                if (modKeySet.Add(modKey))
-                {
-                    System.Console.Write(modKey + " added." + "\n");
-                }
+                //add keys to our hashset
+                modKeySet.Add(modKey);
+                modKeySet.Add(linkedKeys);
             }
 
             //detect GMST records in mods, adds mods containing those records to set
             foreach (var gmstGetter in state.LoadOrder.PriorityOrder.GameSetting().WinningOverrides())
             {
                 var modKey = gmstGetter.FormKey.ModKey;
+                var linkedKeys = gmstGetter.LinkFormKeys.Select(l => l.ModKey);
 
-                if (modKeySet.Add(modKey))
-                {
-                    System.Console.Write(modKey + " added." + "\n");
-                }
+                //add keys to our hashset
+                modKeySet.Add(modKey);
+                modKeySet.Add(linkedKeys);
             }
 
             //detect npc records in mods, adds mods containing those records to set
             foreach (var npcGetter in state.LoadOrder.PriorityOrder.Npc().WinningOverrides())
             {
                 var modKey = npcGetter.FormKey.ModKey;
+                var linkedKeys = npcGetter.LinkFormKeys.Select(l => l.ModKey);
 
-                if (modKeySet.Add(modKey))
-                {
-                    System.Console.Write(modKey + " added." + "\n");
-                }
+                //add keys to our hashset
+                modKeySet.Add(modKey);
+                modKeySet.Add(linkedKeys);
+            }
+
+            //removes null mod names that may have been gathered
+            modKeySet.Remove(ModKey.Null);
+
+            //list collected masters
+            foreach (ModKey key in modKeySet)
+            {
+                System.Console.WriteLine(key);
             }
 
             //shows total number of mods we're going to have as masters
@@ -149,7 +160,7 @@ namespace HeartSeekerUPFLoader
             var dataPath = Path.Combine(GameRelease.SkyrimSE.ToWjGame().MetaData().GameLocation().ToString(), "Data");
 
             //old load order getter
-            var myLoadOrder = state.LoadOrder.Select(Entry => Entry.Key);
+            var myLoadOrder = state.LoadOrder.Select(loadOrder => loadOrder.Key);
 
 
             //takes the set of mods we've collected and adds them as masters to the esp
@@ -167,7 +178,7 @@ namespace HeartSeekerUPFLoader
             {
                 // Don't modify the content of the masters list with what records we have inside
                 MastersListContent = BinaryWriteParameters.MastersListContentOption.NoCheck,
-                
+                 
                 // Order the masters to match load order
                 //old load order getter config
                 MastersListOrdering = new BinaryWriteParameters.MastersListOrderingByLoadOrder(myLoadOrder),
